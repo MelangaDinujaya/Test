482db44f902296bc0529c0b63567bd351d6d6d98
MelangaDinujaya <99182147+MelangaDinujaya@users.noreply.github.com>
2025-10-19 22:15:16 +0530

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,11 @@
-
 import logo from '../assets/OW-Logo-RGB.svg'
 
 const Logo = () => {
-    return (
-        <div className='w-3/4 md:py-5 lg:w-60 h-17 px-5'>
-            <img src={logo} alt='logo' className='w-full h-full object-contain' />
-        </div>
-    )
+  return (
+    <div className='w-3/4 md:py-5 lg:w-60 h-17 px-5'>
+      <img src={logo} alt='logo' className='w-full h-full object-contain' />
+    </div>
+  )
 }
-
 
 export default Logo
