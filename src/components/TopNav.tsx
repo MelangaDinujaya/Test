3affe3d782742b2fca4337ab0ae142eafd45b5f5
MelangaDinujaya <99182147+MelangaDinujaya@users.noreply.github.com>
2025-10-19 22:29:51 +0530

    Update TopNav.tsx

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,3 +1,4 @@
+import Logo from './Logo'
 import NavLink from './NavLink'
 import NavOptions from './NavOptions'
 
@@ -11,18 +12,23 @@ const navItems = [
 
 const TopNav = () => {
   return (
-    <div className='flex items-center justify-between w-full px-5 py-1 bg-blue-700 lg:bg-transparent lg:relative fixed top-0 left-0 z-50'>
+    <div className='flex items-center justify-between w-full px-5 py-3 bg-blue-700 lg:bg-transparent fixed top-0 left-0 z-50'>
       
-      {/* Book an eye test button */}
-      <button className='uppercase cursor-pointer text-sm font-semibold bg-white border-blue-700 text-blue-700 border rounded-2xl px-10 py-1'>
-        Book an eye test
-      </button>
+      {/* Logo */}
+      <Logo />
 
-      {/* Navigation links */}
+      {/* Navigation Links */}
       <div className='hidden lg:flex items-center gap-4'>
         {navItems.map((item) => (
           <NavLink key={item.label} label={item.label} path={item.path} isActive={false} />
         ))}
+      </div>
+
+      {/* Book an Eye Test Button */}
+      <div className='hidden lg:block'>
+        <button className='uppercase cursor-pointer text-sm font-semibold bg-white border-blue-700 text-blue-700 border rounded-2xl px-5 py-2'>
+          Book an Eye Test
+        </button>
       </div>
 
       {/* Icons */}
