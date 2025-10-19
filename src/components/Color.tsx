b182baeb1714050a71a4b2a8fef57d1e3529f87b
MelangaDinujaya <99182147+MelangaDinujaya@users.noreply.github.com>
2025-10-19 22:14:50 +0530

    Update Color.tsx

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,17 +1,18 @@
-
 type Props = {
-    onChange : (col: string) => void
-    isActive : boolean
-    color : string
+  onChange: (col: string) => void
+  isActive: boolean
+  color: string
 }
 
-const Color = (props: Props) => {
-    return (
-        <div onClick={() => props.onChange(props.color)} className={`cursor-pointer border ${props.isActive ? ' border-gray-500' : 'border-gray-300'} w-fit p-1 rounded-full`}>
-            <div className='w-6 h-6 rounded-full' style={{ backgroundColor: props.color }}></div>
-        </div>
-    )
+const Color = ({ onChange, isActive, color }: Props) => {
+  return (
+    <div
+      onClick={() => onChange(color)}
+      className={`cursor-pointer border ${isActive ? 'border-gray-500' : 'border-gray-300'} w-fit p-1 rounded-full`}
+    >
+      <div className='w-6 h-6 rounded-full' style={{ backgroundColor: color }}></div>
+    </div>
+  )
 }
-
 
 export default Color
